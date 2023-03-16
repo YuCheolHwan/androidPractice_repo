efb3251c5f9f18f2cbd83f9d58010d416beef7e2
YuCheolHwan <cheolhwan0816@gmail.com>
2023-03-16 17:47:59 +0900

    [create] second day practice file upload

--- app/src/main/java/com/example/andoridfirst/MainActivity.kt
+++ app/src/main/java/com/example/andoridfirst/MainActivity.kt
@@ -1,28 +1,47 @@
 package com.example.andoridfirst
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.InputType
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.andoridfirst.databinding.ActivitySub2Binding
-import com.example.andoridfirst.databinding.ActivitySub3Binding
+import com.example.andoridfirst.databinding.*
 
 class MainActivity : AppCompatActivity() {
-        lateinit var binding : ActivitySub2Binding
-        var flag : Boolean = false
+    lateinit var binding: ActivitySub19Binding
+//    lateinit var binding2: ActivitySub12Binding
+//    lateinit var binding3: ActivitySub11Binding
+    var flag: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySub2Binding.inflate(layoutInflater)
+        binding = ActivitySub19Binding.inflate(layoutInflater)
+//        binding2 = ActivitySub12Binding.inflate(layoutInflater)
+//        binding3 = ActivitySub11Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnClick.setOnClickListener {
-            Toast.makeText(applicationContext,"유철환님 반갑습니다",Toast.LENGTH_SHORT).show()
-            if(flag == true){
-            binding.tvClassName.text = "안웁 3기"
-                flag = false
-            }else{
-            binding.tvClassName.text = "철환 3기"
-            flag = true
-            }
-        }
-
+//        binding.tvLongClick.setOnLongClickListener{
+//            binding.edtPw.inputType = InputType.TYPE_TEXT_VARIATION_NORMAL
+//            Toast.makeText(applicationContext,binding.edtPw.text.toString(),Toast.LENGTH_SHORT).show()
+//            Handler(Looper.getMainLooper()).postDelayed({
+//
+//            },2000)
+//            return@setOnLongClickListener(true)
+//        }
+//        binding.edtPw.setOnClickListener{
+//            binding.edtPw.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+//        }
+//        binding.btnClick.setOnClickListener {
+//        if(flag == true){
+//            binding.btnTarget.visibility = View.VISIBLE
+//            binding.btnGone.visibility = View.VISIBLE
+//            flag = false
+//        }else{
+//            binding.btnTarget.visibility = View.INVISIBLE
+//            binding.btnGone.visibility = View.GONE
+//            flag = true
+//        }
+//        }
     }
+
 }
--- app/src/main/java/com/example/andoridfirst/SubActivity5.kt
+++ app/src/main/java/com/example/andoridfirst/SubActivity5.kt
@@ -2,10 +2,16 @@ package com.example.andoridfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.andoridfirst.databinding.ActivitySub5Binding
 
 class SubActivity5 : AppCompatActivity() {
+    lateinit var binding : ActivitySub5Binding
+    var flag : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub5)
+        binding = ActivitySub5Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.text = "바꿨다"
     }
 }
